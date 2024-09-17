49c168996a3b5dd28495b9ea6944b6affa531477
WhiteBeard <PTgamers911@gmail>
2024-09-17 20:24:00 +0530

    first

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def home(requests):
+    # return HttpResponse('Hello world')
+    return render(requests, "base.html")
